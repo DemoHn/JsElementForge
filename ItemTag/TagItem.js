0712ca9d801985e8e632e31a6d52251e669443a1
卢名川 <601790183@qq.com>
2013-05-19 21:40:21 +0800

    first commit

---
+++ ItemTag/TagItem.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Hn
+ * Date: 13-4-30
+ * Time: 上午12:50
+ * To change this template use File | Settings | File Templates.
+ */
+var TagItem=function(parent_id){
+    var style={
+        Height:"40px",
+        Width:"100px"
+    };
+    return{
+
+    }
+};
---
+++ UploadButton/Code/AdjustFileInput.js
@@ -0,0 +1,17 @@
+/*Writteb by DemoHn*/
+(function(){
+    var Height;
+    var Width;
+
+    function AdjustHeight(parent_id){
+
+    }
+
+    $(document).ready(function(){
+        var parent_id="DefaultIndentify";
+        var fs=$("#"+parent_id);
+        Height=fs.height();
+        Width=fs.width();
+    })
+})();
+
